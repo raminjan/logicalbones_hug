5f08cd96bbde640959f4c810091e3aa35e5bd6d8
karmatosed <karmatosed@gmail.com>
2012-09-20 22:07:21 +0100

    Content fixes

--- content-attachment.php
+++ content-attachment.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Single post view
+ * Attachment
  *
- * @package logicalboneshug
- * @since logicalboneshug 1.0
+ * @package thatcamp
+ * @since thatcamp 1.0
  */
 ?>
 <?php do_action( 'bp_before_blog_post' ); ?>
@@ -12,18 +12,19 @@
 		<h1 class="post-title"><?php the_title(); ?></h1>
 	</header>
 	<div class="post-body">
+		<?php echo wp_get_attachment_image( $post->ID, 'large', false, array( 'class' => 'size-large aligncenter' ) ); ?>
+		<div class="post-caption"><?php if ( !empty( $post->post_excerpt ) ) the_excerpt(); ?></div>
 		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'logicalboneshug'), 'after' => '</div>' ) ); ?>
 	</div>
 	<footer class="post-meta">
 			<div class="post-meta-author">
 				<div class="author-info">
 					<div class="author-box">
 						<?php echo get_avatar( get_the_author_meta( 'user_email' ), '50' ); ?>
-						<p><?php printf( _x( 'by %s', 'Post written by...', 'logicalboneshug' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
+						<p><?php printf( _x( 'by %s', 'Post written by...', 'thatcamp' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
 					</div>
 					<div class="author-description">
-						<h3><?php printf( __( 'Author: %s', 'logicalboneshug'), "<a href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a>" ); ?></h3>
+						<h3><?php printf( __( 'Author: %s', 'thatcamp'), "<a href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a>" ); ?></h3>
 					</div>
 				</div>
 			</div>
@@ -31,20 +32,24 @@
 			<?php echo get_the_date(); ?></div>
 				<?php if ( comments_open() && ! post_password_required() ) : ?>
 			<div class="comments-link">
-				<?php comments_popup_link( __( '<span class="leave-reply">Comment</span>', 'logicalboneshug'), __( '1 Comment', 'logicalboneshug'), __( '% Comments', 'logicalboneshug') ); ?>
+				<?php comments_popup_link( __( '<span class="leave-reply">Comment</span>', 'thatcamp'), __( '1 Comment', 'thatcamp'), __( '% Comments', 'thatcamp') ); ?>
 			</div>
 		<?php endif; ?>
-		<div class="post-categories">
-			<?php _e( 'Categories: ', 'logicalboneshug'); ?><?php the_category( ' ' ); ?>
-		</div>
-		<div class="post-tags">
-			<?php $tags_list = get_the_tag_list( '', ', ' ); 
-			if ( $tags_list ): ?>
-			<?php printf( __( 'Tags: %2$s', 'logicalboneshug'), 'tag-links', $tags_list ); ?> | 
-			<?php endif; ?>
-		</div>
-			<?php edit_post_link( __( 'Edit &rarr;', 'logicalboneshug'), ' <span class="edit-link">', '</span> | ' ); ?>
-			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'logicalboneshug'), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php _e( 'Permlink', 'logicalboneshug'); ?></a>
+<?php
+									if ( wp_attachment_is_image() ) :
+										$metadata = wp_get_attachment_metadata();
+										printf( __( 'Full size is %s pixels', 'thatcamp' ),
+											sprintf( '<a href="%1$s" title="%2$s">%3$s &times; %4$s</a>',
+												wp_get_attachment_url(),
+												esc_attr( __( 'Link to full size image', 'thatcamp' ) ),
+												$metadata['width'],
+												$metadata['height']
+											)
+										);
+									endif;
+								?>
+			<?php edit_post_link( __( 'Edit &rarr;', 'thatcamp'), ' <span class="edit-link">', '</span> | ' ); ?>
+			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'thatcamp'), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php _e( 'Permlink', 'thatcamp'); ?></a>
 	</footer>
 </article>
-<?php do_action( 'bp_after_blog_post' ); ?>
+<?php do_action( 'bp_after_blog_single_post' ); ?>
--- content-single.php
+++ content-single.php
@@ -13,28 +13,28 @@
 	</header>
 	<div class="post-body">
 		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'testtheme' ), 'after' => '</div>' ) ); ?>
 	</div>
 	<footer class="post-meta">
 			<div class="post-meta-author">
 				<div class="author-info">
 					<div class="author-box">
 						<?php echo get_avatar( get_the_author_meta( 'user_email' ), '50' ); ?>
-						<p><?php printf( _x( 'by %s', 'Post written by...', 'logicalboneshug' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
+						<p><?php printf( _x( 'by %s', 'Post written by...', 'thatcamp' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
 					</div>
 					<div class="author-description">
-						<h3><?php printf( __( 'Author: %s', 'logicalboneshug'), "<a href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a>" ); ?></h3>
+						<h3><?php printf( __( 'Author: %s', 'thatcamp'), "<a href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a>" ); ?></h3>
 					</div>
 				</div>
 			</div>
 		<div class="post-date">
 			<?php echo get_the_date(); ?></div>
-			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+				<?php if ( comments_open() && ! post_password_required() ) : ?>
 			<div class="comments-link">
-				<?php comments_popup_link( __( '<span class="leave-reply">Comment</span>', 'logicalboneshug'), __( '1 Comment', 'logicalboneshug'), __( '% Comments', 'logicalboneshug') ); ?>
+				<?php comments_popup_link( __( '<span class="leave-reply">Comment</span>', 'thatcamp'), __( '1 Comment', 'thatcamp'), __( '% Comments', 'thatcamp') ); ?>
 			</div>
-			<?php edit_post_link( __( 'Edit &rarr;', 'logicalboneshug'), ' <span class="edit-link">', '</span> | ' ); ?>
-			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'logicalboneshug'), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php _e( 'Permlink', 'logicalboneshug'); ?></a>
+		<?php endif; ?>
+			<?php edit_post_link( __( 'Edit &rarr;', 'thatcamp'), ' <span class="edit-link">', '</span> | ' ); ?>
+			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'thatcamp'), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php _e( 'Permlink', 'thatcamp'); ?></a>
 	</footer>
 </article>
 <?php do_action( 'bp_after_blog_single_post' ); ?>
